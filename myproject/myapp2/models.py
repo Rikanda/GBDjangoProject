4fad14a002202410be6f8457636695e12fb52b29
Natalya <soltk7@yandex.ru>
2024-03-30 21:02:21 +0300

    second lesson

--- myproject/myapp2/models.py
+++ myproject/myapp2/models.py
@@ -1,26 +1,28 @@
 from django.db import models
 
 
-# class Model(models.Model):
-#     objects = models.Manager()
+class Model(models.Model):
+    objects = models.Manager()
+    class Meta:
+        abstract = True
 
 
 # Create your models here.
-class User(models.Model):
+class User(Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     password = models.CharField(max_length=100)
     age = models.IntegerField()
 
 
-class Product(models.Model):
+class Product(Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
     image = models.ImageField(upload_to='products/')
 
 
-class Order(models.Model):
+class Order(Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     date_ordered = models.DateTimeField(auto_now_add=True)
