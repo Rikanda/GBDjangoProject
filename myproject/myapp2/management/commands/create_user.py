738b771f75c2b04db71704054b2a4681e3706c99
Natalya <soltk7@yandex.ru>
2024-04-01 19:02:02 +0300

    second lesson

--- myproject/myapp2/management/commands/create_user.py
+++ myproject/myapp2/management/commands/create_user.py
@@ -1,12 +1,11 @@
 from django.core.management.base import BaseCommand
-import myproject
-from myapps2.models import User
+from myapp2.models import User
 
 
 class Command(BaseCommand):
     help = "Create user"
 
     def handle(self, *args, **kwargs):
-        user = User(name = 'Jorgh', email = 'jo@example.com', password = 'secret', age = 25)
+        user = User(name = 'Jack', email = 'jo@example.com', password = 'secret', age = 60)
         user.save()
         self.stdout.write(f'{user}')
--- myproject/myapp2/models.py
+++ myproject/myapp2/models.py
@@ -1,28 +1,31 @@
 from django.db import models
 
-
-class Model(models.Model):
-    objects = models.Manager()
-    class Meta:
-        abstract = True
+# class Model(models.Model):
+#     objects = models.Manager()
+#     class Meta:
+#         abstract = True
 
 
 # Create your models here.
-class User(Model):
+
+class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     password = models.CharField(max_length=100)
     age = models.IntegerField()
 
+    def __str__(self):
+        return f'Username: {self.name}, email: {self.email}, age: {self.age}'
 
-class Product(Model):
+
+class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
     image = models.ImageField(upload_to='products/')
 
 
-class Order(Model):
+class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     date_ordered = models.DateTimeField(auto_now_add=True)
